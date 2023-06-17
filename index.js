4f385e6437795f825a1e2446dcf0aaeec54413a4
Benjamin <benjamincliminio@gmail.com>
2023-06-17 21:08:28 +0200

    Finish To The Proyect(17/06/23)

--- index.js
+++ index.js
@@ -1,77 +1,98 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const inputArea = document.getElementById("inputArea");
-    const button = document.getElementById("addBtn");
-    const list = document.getElementById("tasks-container");
-  
-    function inputLength() {
-      return inputArea.value.length;
+  const inputArea = document.getElementById("inputArea");
+  const button = document.getElementById("addBtn");
+  const tasksContainer = document.getElementById("tasks-container");
+  const inProgressContainer = document.getElementById("in-progress");
+  const finishedContainer = document.getElementById("finished");
+
+  function inputLength() {
+    return inputArea.value.length;
+  }
+
+  let taskId = 0;
+
+  function createTaskElement(text) {
+    const li = document.createElement("li");
+    li.textContent = text;
+    li.setAttribute("draggable", "true");
+    li.setAttribute("id", "task-" + taskId++);
+    li.style.marginLeft = "20px";
+
+    function crossOut() {
+      li.classList.toggle("done");
     }
-  
-    let x = 0;
-  
-    function createListElement() {
-      const li = document.createElement("li");
-      li.appendChild(document.createTextNode(inputArea.value));
-      li.style.marginLeft = "20px";
-      list.appendChild(li);
-      li.setAttribute("draggable", "true");
-      li.setAttribute("ondragstart", "drag(event)");
-      li.setAttribute("id", "task-" + (x += 1));
+
+    li.addEventListener("click", crossOut);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", deleteListItem);
+
+    function deleteListItem() {
+      li.classList.add("delete");
+    }
+
+    return li;
+  }
+
+  function addTaskToList(list, taskElement) {
+    list.appendChild(taskElement);
+    taskElement.addEventListener("dragstart", dragStart);
+    taskElement.addEventListener("dragend", dragEnd);
+  }
+
+  function createListElement() {
+    const taskText = inputArea.value.trim();
+    if (taskText.length > 0) {
+      const taskElement = createTaskElement(taskText);
+      addTaskToList(tasksContainer, taskElement);
       inputArea.value = "";
-  
-      function crossOut() {
-        li.classList.toggle("done");
-      }
-  
-      li.addEventListener("click", crossOut);
-  
-      const deleteBtn = document.createElement("button");
-      deleteBtn.appendChild(document.createTextNode("X"));
-      li.appendChild(deleteBtn);
-      deleteBtn.addEventListener("click", deleteListItem);
-  
-      function deleteListItem() {
-        li.classList.add("delete");
-      }
     }
-  
-    function addListAfterClick() {
-      if (inputLength() > 0) {
-        createListElement();
-      }
+  }
+
+  function addListAfterClick() {
+    if (inputLength() > 0) {
+      createListElement();
     }
-  
-    function addListAfterKeypress(event) {
-      if (inputLength() > 0 && event.which === 13) {
-        createListElement();
-      }
+  }
+
+  function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.which === 13) {
+      createListElement();
     }
-  
-    button.addEventListener("click", addListAfterClick);
-  
-    inputArea.addEventListener("keypress", addListAfterKeypress);
-  
-    function allowDrop(ev) {
-      ev.preventDefault();
-    }
-  
-    function drag(ev) {
-      ev.dataTransfer.setData("text", ev.target.id);
-    }
-  
-    function drop(ev) {
-      ev.preventDefault();
-      const data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(data));
-    }
-  
-    const inProgressContainer = document.getElementById("in-proccess");
-    const finishedContainer = document.getElementById("finished");
-  
-    inProgressContainer.addEventListener("dragover", allowDrop);
-    inProgressContainer.addEventListener("drop", drop);
-  
-    finishedContainer.addEventListener("dragover", allowDrop);
-    finishedContainer.addEventListener("drop", drop);
-  });
-  
+  }
+
+  button.addEventListener("click", addListAfterClick);
+  inputArea.addEventListener("keypress", addListAfterKeypress);
+
+  function dragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.target.classList.add("dragging");
+  }
+
+  function dragEnd(event) {
+    event.target.classList.remove("dragging");
+  }
+
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    const taskId = event.dataTransfer.getData("text/plain");
+    const taskElement = document.getElementById(taskId);
+    const targetContainer = event.currentTarget;
+    targetContainer.appendChild(taskElement);
+  }
+
+  tasksContainer.addEventListener("dragover", allowDrop);
+  tasksContainer.addEventListener("drop", drop);
+
+  inProgressContainer.addEventListener("dragover", allowDrop);
+  inProgressContainer.addEventListener("drop", drop);
+
+  finishedContainer.addEventListener("dragover", allowDrop);
+  finishedContainer.addEventListener("drop", drop);
+});
